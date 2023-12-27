f87f695aa5b21551ab5da04827cb1ad72d438d2a
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-27 16:20:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+ const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled'];
 
-  // Function to strip certain words from the beginning for sorting
-  function stripWords(bandName) {
-    const ignoreWords = ['The', 'A', 'An'];
-    const words = bandName.split(' ');
-    if (ignoreWords.includes(words[0])) {
-      words.shift();
-    }
-    return words.join(' ');
+  // Function to remove specified words from a string
+  function removeCommonWords(str) {
+    const commonWords = ['a', 'an', 'the'];
+    const regex = new RegExp('\\b(' + commonWords.join('|') + ')\\b', 'gi');
+    return str.replace(regex, '').trim();
   }
 
-  // Sort the bands
-  const sortedBands = bands.slice().sort((a, b) => stripWords(a) > stripWords(b) ? 1 : -1);
-
-  // Display the sorted bands
-  document.write('<ul>');
-  sortedBands.forEach(band => {
-    document.write(`<li>${band}</li>`);
+  // Sort the bands without considering common words
+  const sortedBands = bands.sort((a, b) => {
+    const bandA = removeCommonWords(a);
+    const bandB = removeCommonWords(b);
+    return bandA.localeCompare(bandB);
   });
-  document.write('</ul>');
+
+  // Display the sorted bands on the webpage
+  const bandList = document.getElementById('bands');
+  sortedBands.forEach(band => {
+    const listItem = document.createElement('li');
+    listItem.textContent = band;
+    bandList.appendChild(listItem);
+  });
