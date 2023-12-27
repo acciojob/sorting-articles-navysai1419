5819b8b0a2fdc6058897dd5bbe5a2cdf0848d04a
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-27 15:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-//your JS code here. If required.
- const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
-    // Function to remove specified words from a string
-    function removeWords(str, wordsToRemove) {
-        const regex = new RegExp('\\b(' + wordsToRemove.join('|') + ')\\b', 'gi');
-        return str.replace(regex, '').trim();
+  // Function to strip certain words from the beginning for sorting
+  function stripWords(bandName) {
+    const ignoreWords = ['The', 'A', 'An'];
+    const words = bandName.split(' ');
+    if (ignoreWords.includes(words[0])) {
+      words.shift();
     }
+    return words.join(' ');
+  }
 
-    // Sorting the bands without considering specified words
-    const sortedBands = bands.sort((a, b) => {
-        const bandA = removeWords(a, ['a', 'an', 'the']);
-        const bandB = removeWords(b, ['a', 'an', 'the']);
-        return bandA.localeCompare(bandB);
-    });
+  // Sort the bands
+  const sortedBands = bands.slice().sort((a, b) => stripWords(a) > stripWords(b) ? 1 : -1);
 
-    // Displaying the sorted bands on the webpage
-    const bandList = document.getElementById('band');
-    sortedBands.forEach(band => {
-        const listItem = document.createElement('li');
-        listItem.textContent = band;
-        bandList.appendChild(listItem);
-    });
+  // Display the sorted bands
+  document.write('<ul>');
+  sortedBands.forEach(band => {
+    document.write(`<li>${band}</li>`);
+  });
+  document.write('</ul>');
